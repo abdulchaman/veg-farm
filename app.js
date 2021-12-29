f860086e71d6ca0b40eab138fb03accf071d2d6f
aziz <abdulchaman123@gmail.com>
2021-12-30 00:56:37 +0530

    UI add

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 const fs = require("fs");
-const https = require("https");
+const http = require("http");
+const url = require("url");
+const server = http.createServer((req,res)=>{
+    const pathname = req.url;
+    console.log(pathname)
+    res.write("Hello world");
+    res.end();
+});
+server.listen(8000,()=>{
+    console.log("server has started")
+})
